f058f60fa9632c1b4b0e1ad630a40eb73bd80e4f
Guadalupe Bernal <guadabernal@gmail.com>
2023-12-14 21:50:39 -0500

    update

--- sequenceObserver.py
+++ sequenceObserver.py
@@ -1,9 +1,12 @@
 import json
 import cv2
 import numpy as np
-from scipy.optimize import linear_sum_assignment
-from scipy.spatial.distance import cdist
 import time
+from scipy.spatial.distance import cdist
+
+# Change video path
+video_path = 'videos_avi/01_2023-08-02_18-29-59.avi'
+json_seq_path = video_path.replace('.avi', '_seq.json')
 
 def calculate_distance(p1, p2):
     return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
@@ -47,17 +50,13 @@ def pathTracking(sequence):
 
     return bees_tracks
 
-video_path = 'videos/avi/01_2023-08-02_18-29-59.avi'
-json_seq_path = video_path.replace('.avi', '_seq.json')
-
 with open(json_seq_path, 'r') as file:
     sequences = json.load(file)
 
 enter_sequences = sequences
-#[seq for seq in sequences if seq['type'] == 'guard']
 
 cap = cv2.VideoCapture(video_path)
-
+play_delay = 0
 
 colors = [
     (180, 0, 0),  # Red
@@ -67,6 +66,7 @@ colors = [
     (255, 0, 255),  # Magenta
     (0, 255, 255)  # Yellow
 ]
+
 id = 0
 while 0 <= id < len(enter_sequences):
     seq = enter_sequences[id]
@@ -83,7 +83,6 @@ while 0 <= id < len(enter_sequences):
             ret, frame = cap.read()
             frame_idx = (frame_idx + 1) % len(frames)
         else:
-            # Set to the first frame only when not in playing mode
             cap.set(cv2.CAP_PROP_POS_FRAMES, frames[0])
             ret, frame = cap.read()
 
@@ -99,51 +98,58 @@ while 0 <= id < len(enter_sequences):
                 pt2 = (int(path[i][0]), int(path[i][1]))
                 cv2.line(frame, pt1, pt2, bee_color, 2, cv2.LINE_AA)
 
-            # Draw the starting and ending points with a colored border
             if path:
                 first_point = (int(path[0][0]), int(path[0][1]))
                 last_point = (int(path[-1][0]), int(path[-1][1]))
 
-                # Draw the starting point (green with a colored border)
-                cv2.circle(frame, first_point, 7, bee_color, -1)  # Colored border
-                cv2.circle(frame, first_point, 5, (0, 255, 0), -1)  # Green for the start
+                # draw the starting point (green with a colored border)
+                cv2.circle(frame, first_point, 7, bee_color, -1)
+                cv2.circle(frame, first_point, 5, (0, 255, 0), -1)
 
-                # Draw the ending point (red with a colored border)
-                cv2.circle(frame, last_point, 7, bee_color, -1)  # Colored border
-                cv2.circle(frame, last_point, 5, (0, 0, 255), -1)  # Red for the end
+                # draw the ending point (red with a colored border)
+                cv2.circle(frame, last_point, 7, bee_color, -1)
+                cv2.circle(frame, last_point, 5, (0, 0, 255), -1)
 
         sequence_info = f'ID: {id}/{len(enter_sequences)}, SeqId: {seq["sequence_id"]}, Length: {len(seq["data"])}, Type: {seq["type"]}'
         cv2.putText(frame, sequence_info, (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
-
         cv2.imshow('Sequence Visualization', frame)
+        if play_delay != 0:
+            time.sleep(play_delay)
 
         if video_writer is not None:
             video_writer.write(frame)
 
-        key = cv2.waitKey(1 if playing else 0)  # Wait only 1 ms if playing
+        key = cv2.waitKey(1 if playing else 0)  # wait only 1 ms if playing
 
         if key == ord('q'):
             break
         elif key == ord('b') and not playing:  # 'b' key for going back
             id = max(0, id - 1)
             break
+        elif key == ord('w') and not playing:  # 'w' speed up playback
+            play_delay = max(0, play_delay - 0.005) # 5 ms
+            print(play_delay)
+            break
+        elif key == ord('s') and not playing:  # 's' slow down playback
+            play_delay = min(1, play_delay + 0.005) # 5 ms
+            print(play_delay)
+            break
         elif key == ord('n') and not playing:  # 'n' key for next sequence
             id += 1
             break
-        elif key == ord('p'):  # 'p' key to play/pause the sequence
-            playing = not playing  # Toggle the play state
-        elif key == ord('d'): # dump frames and boxes
+        elif key == ord('p'):                   # 'p' key to play/pause the sequence
+            playing = not playing                   # Toggle the play state
+        elif key == ord('g'):                   # dump frames and boxes
             for s in seq['data']:
                 print(s['frame'], s['boxes'])
-        elif key == ord('s') and not playing:  # 's' key to save the sequence
+        elif key == ord('d') and not playing:   # 's' key to save the sequence
             if video_writer is None:
-                # Initialize video writer
                 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                fps = cap.get(cv2.CAP_PROP_FPS)
+                # fps = cap.get(cv2.CAP_PROP_FPS)
+                fps = 5
                 frame_size = (frame.shape[1], frame.shape[0])
                 video_writer = cv2.VideoWriter('output.avi', fourcc, fps, frame_size)
             else:
-                # Save and close the video writer and reset it for next use
                 video_writer.release()
                 video_writer = None
         if playing:
